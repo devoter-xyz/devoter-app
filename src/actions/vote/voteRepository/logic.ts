4e669db3e6e8cacc0cce713f70d71f97126d6582
Jesica Mao <jesica.mao42@gmail.com>
2025-07-16 04:41:58 -0700

    voteRepo transaction check

--- src/actions/vote/voteRepository/logic.ts
+++ src/actions/vote/voteRepository/logic.ts
@@ -6,16 +6,34 @@ import { getWeek } from '@/lib/utils/date';
 import { Decimal } from '@prisma/client/runtime/library';
 import { getTokenBalance } from '../../user/getTokenBalance/logic';
 import { VoteRepositoryInput } from './schema';
+import { publicClient } from '@/lib/viem';
+import { InvalidTransactionError } from '@/lib/errors';
 
 export const voteRepository = async (input: VoteRepositoryInput, userId: string) => {
   const currentWeek = getWeek(new Date());
 
-  await prisma.$transaction(async tx => {
-    const user = await tx.user.findUniqueOrThrow({
-      where: { id: userId },
-      select: { walletAddress: true }
-    });
+  const transaction = await publicClient.getTransaction({
+    hash: input.txHash as `0x${string}`
+  });
 
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id: userId },
+    select: { walletAddress: true }
+  });
+
+  if (!transaction || transaction.from.toLowerCase() !== user.walletAddress.toLowerCase()) {
+    throw new InvalidTransactionError('Invalid transaction.');
+  }
+
+  const block = await publicClient.getBlock({ blockHash: transaction.blockHash });
+  const transactionTime = new Date(Number(block.timestamp) * 1000);
+  const oneMinuteAgo = new Date(Date.now() - 60 * 1000);
+
+  if (transactionTime < oneMinuteAgo) {
+    throw new InvalidTransactionError('Transaction is too old.');
+  }
+
+  await prisma.$transaction(async tx => {
     const balance = await getTokenBalance(user.walletAddress);
     const userBalance = new Decimal(balance || '0');
     const tokenAmount = userBalance.mul(0.0025);
