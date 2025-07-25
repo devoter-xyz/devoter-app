38937db78e8d94d8f4edf849661e59f497fe22ba
Jack Ulf <metanodedreamer@gmail.com>
2025-07-25 15:01:41 -0600

    used primary color in all instances

--- src/components/common/Sidebar/SidebarItem.tsx
+++ src/components/common/Sidebar/SidebarItem.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import { LucideIcon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { LucideIcon } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
@@ -21,12 +21,12 @@ export const SidebarItem = ({ href, icon: Icon, text }: SidebarItemProps) => {
       <Link href={href}>
         <div
           className={cn(
-            'flex items-center p-3 rounded-lg text-black hover:bg-purple-100 dark:hover:bg-purple-800/20',
-            isActive && 'bg-purple-100/50 dark:bg-purple-800/30 text-purple-600 dark:text-purple-400 border border-purple-500'
+            'flex items-center p-3 rounded-lg text-black hover:bg-primary/10 dark:hover:bg-primary/20',
+            isActive && 'bg-primary/10 dark:bg-primary/30 text-primary dark:text-primary border border-primary'
           )}
         >
-          <Icon className="w-5 h-5 mr-3" />
-          <span className="font-medium">{text}</span>
+          <Icon className='w-5 h-5 mr-3' />
+          <span className='font-medium'>{text}</span>
         </div>
       </Link>
     </li>
--- src/components/common/Sidebar/SubmitRepositoryButton.tsx
+++ src/components/common/Sidebar/SubmitRepositoryButton.tsx
@@ -5,8 +5,8 @@ import { Plus } from 'lucide-react';
 
 export const SubmitRepositoryButton = () => {
   return (
-    <Button className="w-full bg-gradient-to-r from-violet-500 to-purple-500 text-white hover:from-violet-600 hover:to-purple-600">
-      <Plus className="w-5 h-5 mr-2" />
+    <Button className='w-full'>
+      <Plus className='w-5 h-5' />
       Submit Repository
     </Button>
   );
