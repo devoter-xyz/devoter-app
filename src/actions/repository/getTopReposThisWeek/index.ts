7f16effb200de57cf63973baa6464c4823a06cdb
Lukas Bauer <lukas.bauer.cyp54@gmail.com>
2025-08-19 10:32:44 -0600

    feat: Implement action to retrieve top voted repositories for the current week (#82)

---
+++ src/actions/repository/getTopReposThisWeek/index.ts
@@ -0,0 +1,5 @@
+export { getTopReposThisWeekAction } from './action';
+export { getTopReposThisWeek } from './logic';
+export { getTopReposThisWeekSchema } from './schema';
+export type { GetTopReposThisWeekInput } from './schema';
+export type { TopRepoThisWeek, GetTopReposThisWeekResult } from './logic';
