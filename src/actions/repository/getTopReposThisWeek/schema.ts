68cda3089e7cfb8db8e39069f4b7e739d15df39e
Lukas Bauer <lukas.bauer.cyp54@gmail.com>
2025-08-27 13:05:43 -0600

    .

--- src/actions/repository/getTopReposThisWeek/schema.ts
+++ src/actions/repository/getTopReposThisWeek/schema.ts
@@ -4,4 +4,5 @@ export const getTopReposThisWeekSchema = z.object({
   limit: z.number().min(1).max(50).optional().default(10)
 });
 
+
 export type GetTopReposThisWeekInput = z.infer<typeof getTopReposThisWeekSchema>;
