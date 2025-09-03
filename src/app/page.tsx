d42905aad155787f280935180248b81bff724525
Jack Ulf <metanodedreamer@gmail.com>
2025-09-03 10:30:31 -0600

    Added notification popover (#93)
    * feat: Add Popover component and integrate it into Actions and Search for enhanced user interaction
    
    * removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Header } from '@/components/common/Header';
 import HomePage from '@/components/pages/HomePage/HomePage';
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <HomePage />
     </main>
   );
--- src/components/common/Actions.tsx
+++ src/components/common/Actions.tsx
@@ -1,15 +1,21 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 import { Bell } from 'lucide-react';
 import { ConnectWallet } from './Wallet/ConnectWallet';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
 export function Actions() {
   return (
-    <div className="flex items-center gap-4">
-      <Button variant="outline" size="icon">
-        <Bell className="h-4 w-4" />
-      </Button>
+    <div className='flex items-center gap-4'>
+      <Popover>
+        <PopoverTrigger>
+          <Button variant='outline' size='icon'>
+            <Bell className='h-4 w-4' />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          No new notifications
+        </PopoverContent>
+      </Popover>
       <ConnectWallet />
     </div>
   );
