a577603200db240d7413c23ff4e629c6a29f37cb
Lukas Bauer <lukas.bauer.cyp54@gmail.com>
2025-09-17 09:58:02 -0600

    feat(Sidebar): enhance SubmitRepositoryButton to close dialog on successful submission (#102)

--- src/components/common/Sidebar/SidebarItem.tsx
+++ src/components/common/Sidebar/SidebarItem.tsx
@@ -4,8 +4,6 @@ import { LucideIcon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { cn } from '@/lib/utils';
-
 interface SidebarItemProps {
   href: string;
   icon: LucideIcon;
@@ -20,10 +18,11 @@ export const SidebarItem = ({ href, icon: Icon, text }: SidebarItemProps) => {
     <li>
       <Link href={href}>
         <div
-          className={cn(
-            'flex items-center p-3 rounded-lg text-black hover:bg-primary/10 dark:hover:bg-primary/20',
-            isActive && 'bg-primary/10 dark:bg-primary/30 text-primary dark:text-primary border border-primary'
-          )}
+          className={`flex items-center p-3 rounded-lg border transition-colors
+            ${isActive
+              ? 'bg-primary/10 dark:bg-primary/30 text-primary border-primary'
+              : 'text-black dark:text-primary hover:bg-primary/10 dark:hover:bg-primary/20 border-transparent'}
+          `}
         >
           <Icon className='w-5 h-5 mr-3' />
           <span className='font-medium'>{text}</span>
