732e786133429d24862149a8d6663fcae1b39ed0
Mark Philips <question.mark.eth@gmail.com>
2025-10-23 21:35:08 +0000

    Refactor: Make Badge ranking variants explicit and required

--- src/components/common/Badge.tsx
+++ src/components/common/Badge.tsx
@@ -1,15 +1,21 @@
+import { cn } from '@/lib/utils';
 import { type VariantProps } from 'class-variance-authority';
 
 import { Badge, badgeVariants } from '../ui/badge';
 
-export interface BadgeProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
+  variant: 'first' | 'second' | 'third';
+}
 
 function CustomBadge({ className, variant, ...props }: BadgeProps) {
-  const content = variant === 'first' ? '#1' : variant === 'second' ? '#2' : '#3';
+  const contentMap: Record<BadgeProps['variant'], string> = {
+    first: '#1',
+    second: '#2',
+    third: '#3',
+  };
+  const content = contentMap[variant];
   return (
-    <Badge variant={variant} className={className} {...props}>
+    <Badge className={cn(badgeVariants({ variant }), className)} {...props}>
       {content}
     </Badge>
   );
