c977f8b385844ad5e3c8cfdbab58df7d26824354
Larry Sinclair <larry.sinclair.01.crypto@gmail.com>
2025-11-20 08:54:01 -0700

    Feature: Implement Leaderboard Data Export (#170)
    * feat: Add leaderboard data export functionality
    
    * Improve leaderboard export functionality

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,7 @@ export const MINIMUM_VOTE_TOKEN_AMOUNT = 1;
 export const DEV_TOKEN_ADDRESS = '0x047157cffb8841a64db93fd4e29fa3796b78466c';
 export const DEV_TOKEN_UNISWAP_URL = `https://app.uniswap.org/explore/tokens/base/${DEV_TOKEN_ADDRESS}`;
 export const RECIPIENT_WALLET_ADDRESS = '0x9F1E0E0aA99846B3D15A9D1eA5C09F27FBCf8783';
+export const MAX_EXPORT_FILE_SIZE_BYTES = 10 * 1024 * 1024; // 10 MB
 
 export const PREDEFINED_TAGS = [
   'accessibility',
