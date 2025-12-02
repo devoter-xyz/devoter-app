3e222bad89e86225aeb896100321d3291f5a28e5
Lukas Bauer <lukas.bauer.cyp54@gmail.com>
2025-12-02 08:55:02 +0000

    feat: Enhance FilterSkeleton with featured checkbox and correct button width

--- src/components/common/Filter.tsx
+++ src/components/common/Filter.tsx
@@ -240,8 +240,12 @@ export function FilterSkeleton() {
         <Skeleton className="h-4 w-1/3" />
         <Skeleton className="h-8 w-full" />
       </div>
+      <div className="flex items-center space-x-2">
+        <Skeleton className="h-4 w-4" />
+        <Skeleton className="h-4 w-1/2" />
+      </div>
       <div className="flex justify-end mt-4">
-        <Skeleton className="h-8 w-24" />
+        <Skeleton className="h-8 w-full" />
       </div>
     </Card>
   );
